ad2f85497d4cf44c869fd362e9c0521ab253ff5e
adiko01 <adiko01.aik@gmail.com>
2020-05-15 23:08:09 +0200

    Den Sendern  Bilder hinzugefügt und Screenshorts Aktualisiert

--- plugin.audio.eldoradio/addon.py
+++ plugin.audio.eldoradio/addon.py
@@ -2,16 +2,19 @@
 # -*- coding: utf-8 -*-
 import xbmcplugin,xbmcaddon,xbmcgui,os,sys
 
+addon = xbmcaddon.Addon()
+addon_path = addon.getAddonInfo('path').decode('utf-8')
+icons_path = os.path.join(addon_path,'resources')
 xbmcplugin.setContent(handle=int(sys.argv[1]), content='songs')
 				
-def add_item(url,infolabels,):
-    listitem = xbmcgui.ListItem(infolabels['title'])
+def add_item(url,infolabels,img=''):
+    listitem = xbmcgui.ListItem(infolabels['title'],iconImage=img,thumbnailImage=img)
     listitem.setInfo('audio',infolabels)
     listitem.setProperty('IsPlayable','true')
     xbmcplugin.addDirectoryItem(int(sys.argv[1]),url,listitem)
 
-add_item('http://www.eldoradio.de/broadcast/192.m3u',{'title':'[COLOR orange]eldoradio*[/COLOR] mit 192kbit/s'})
-add_item('http://www.eldoradio.de/broadcast/128.m3u',{'title':'[COLOR orange]eldoradio*[/COLOR] mit 128kbit/s'})
+add_item('http://www.eldoradio.de/broadcast/192.m3u',{'title':'[COLOR orange]eldoradio*[/COLOR] mit 192kbit/s'},os.path.join(icons_path,'icon.png'))
+add_item('http://www.eldoradio.de/broadcast/128.m3u',{'title':'[COLOR orange]eldoradio*[/COLOR] mit 128kbit/s'},os.path.join(icons_path,'icon.png'))
 
 
 xbmcplugin.endOfDirectory( handle=int( sys.argv[ 1 ] ), succeeded=True, updateListing=False, cacheToDisc=True)
